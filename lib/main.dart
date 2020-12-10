690a1c6a28ffba5a52b72fa72078b137ff124025
potatojoayo <potatojoayo@gmail.com>
2020-12-10 16:38:30 +0900

    movie page bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -6,47 +6,47 @@ import 'nav/nav_bar.dart';
 PageBloc pageBloc;
 
 void main() async {
-  pageBloc = PageBloc();
-  runApp(KnuMovieWeb());
+	pageBloc = PageBloc();
+	runApp(KnuMovieWeb());
 }
 
 class KnuMovieWeb extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Knu Movie Web',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.grey,
-        ),
-        home: HomePage());
-  }
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+				title: 'Knu Movie Web',
+				debugShowCheckedModeBanner: false,
+				theme: ThemeData(
+						primarySwatch: Colors.grey,
+				),
+				home: HomePage());
+	}
 }
 
 class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SingleChildScrollView(
-          child: StreamBuilder<Widget>(
-            stream: null,
-            builder: (context, snapshot) {
-              return SafeArea(
-                child: StreamBuilder<Widget>(
-                    initialData: LandingPage(),
-                    stream: pageBloc.page,
-                    builder: (context, AsyncSnapshot<Widget> snapshot) {
-                      return Column(
-                        children: <Widget>[NavBar(pageBloc), snapshot.data],
-                      );
-                    }),
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+				child: Scaffold(
+						backgroundColor: Colors.grey[300],
+						body: SingleChildScrollView(
+								child: StreamBuilder<Widget>(
+										stream: null,
+										builder: (context, snapshot) {
+											return SafeArea(
+													child: StreamBuilder<Widget>(
+															initialData: LandingPage(),
+															stream: pageBloc.page,
+															builder: (context, AsyncSnapshot<Widget> snapshot) {
+																return Column(
+																		children: <Widget>[NavBar(pageBloc), snapshot.data],
+																);
+															}),
+											);
+										},
+								),
+						),
+				),
+		);
+	}
 }
